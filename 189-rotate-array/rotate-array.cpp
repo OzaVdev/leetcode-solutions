03c8a4e2d41d9bf1bbd5f73f6ce98959221b32a8
Oza Vatsal <internshipoza@gmail.com>
2026-09-19 20:02:32 +0530

    Time: 15 ms (5.06%) | Memory: 263.3 MB (48.05%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+           int n = nums.size();
+        k = k % n;
+
+        int count = 0;
+        int start = 0;
+
+        while (count < n) {
+            int current = start;
+            int prev = nums[start];
+
+            do {
+                int next = (current + k) % n;
+
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+
+                current = next;
+                count++;
+
+            } while (current != start);
+
+            start++;
+        }
+    }
+};
