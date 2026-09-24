340f990bed9039835f833f9dba39189f472f47e3
Oza Vatsal <internshipoza@gmail.com>
2026-09-24 20:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (25.64%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int last = digits.size() - 1;
-
-        // Start from the last digit
-        while (last >= 0) {
-
-            // If digit is less than 9, simply add 1
-            if (digits[last] < 9) {
-                digits[last] += 1;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
                 return digits;
             }
 
-            // If digit is 9, it becomes 0
-            digits[last] = 0;
-            last--;
+            digits[i] = 0;
         }
 
-        // If we reach here, all digits were 9
-        // Example: 999 -> 1000
-
-        vector<int> result(digits.size() + 1, 0);
-        result[0] = 1;
-
-        return result;
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
