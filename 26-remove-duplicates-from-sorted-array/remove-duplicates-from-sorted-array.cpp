a257ab28c6c611b86d043985778e74559c1348ee
Oza Vatsal <internshipoza@gmail.com>
2026-09-24 20:22:08 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int write = 0;
 
-        // write tells us where the next unique element should be placed
-        int write = 1;
-
-        // Start from index 1 because nums[0] is always unique
-        for (int read = 1; read < nums.size(); read++) {
-
-            // If current element is different from the previous unique element
-            if (nums[read] != nums[write - 1]) {
-
-                // Copy the new unique element to the write position
-                nums[write] = nums[read];
-
-                // Move write to the next empty position
-                write++;
+        for (int x : nums) {
+            if (write == 0 || nums[write - 1] != x) {
+                nums[write++] = x;
             }
         }
 
-        // write = total number of unique elements
         return write;
     }
 };
