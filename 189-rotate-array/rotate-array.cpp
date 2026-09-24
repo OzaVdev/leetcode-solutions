ce36a6c946268a73fea85b9d893d3de549083433
Oza Vatsal <internshipoza@gmail.com>
2026-09-24 21:05:44 +0530

    Time: 6 ms (24.87%) | Memory: 262.8 MB (64.21%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    if(nums.size()<=k)
+    {
+        k = k % nums.size();
+        
+    }
+    int start = nums.size() - k ;
+    reverse(nums.begin()+start,nums.begin()+start+k);
+    vector<int> nums1;      
+    for(int i = 0;i<k;i++)
+      {
+        int x = nums.back();
+        nums.pop_back();
+        nums1.push_back(x);
+      }
+    nums.insert(nums.begin(),nums1.begin(),nums1.end());
+     
+    }
+};
 // class Solution {
 // public:
 //     void rotate(vector<int>& nums, int k) {
@@ -29,24 +50,3 @@
 //         }
 //     }
 // };
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-    if(nums.size()<=k)
-    {
-        k = k % nums.size();
-        
-    }
-    int start = nums.size() - k ;
-    reverse(nums.begin()+start,nums.begin()+start+k);
-    vector<int> nums1;      
-    for(int i = 0;i<k;i++)
-      {
-        int x = nums.back();
-        nums.pop_back();
-        nums1.push_back(x);
-      }
-    nums.insert(nums.begin(),nums1.begin(),nums1.end());
-     
-    }
-};
